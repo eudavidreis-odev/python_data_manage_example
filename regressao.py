d2a8dd086482c831439e46835cb41a9edbeee9bd
David Reis <eudavidreis.dev@gmail.com>
2024-05-07 20:23:38 -0300

    Update - Corrigindo código descontinuado.

--- regressao.py
+++ regressao.py
@@ -1,31 +1,33 @@
 import matplotlib.pyplot as plt
 from sklearn.linear_model import LinearRegression
-import pandas
- 
+import pandas as pd
+
 ############# Pré-processamento ###############
 # Coleta e Integração
-arquivo = pandas.read_csv('dados_dengue.csv')
+arquivo = pd.read_csv('dados_dengue.csv')
 
 anos = arquivo[['ano']]
 casos = arquivo[['casos']]
- 
+
 ############## Mineração #################
 regr = LinearRegression()
-regr.fit(X=anos, y=casos)
- 
+regr.fit(anos, casos)  # Ajustando o modelo sem fornecer nomes de características
+
 ano_futuro = [[2018]]
-casos_2018 = regr.predict(ano_futuro)
- 
-print('Casos previstos para 2018 ->', int(casos_2018))
- 
+casos_2018 = regr.predict(ano_futuro)[0]
+
+print('Casos previstos para 2018 ->', int(casos_2018[0]))
+
 ############ Pós-processamento ################
 plt.scatter(anos, casos, color='black')
 plt.scatter(ano_futuro, casos_2018, color='red')
 plt.plot(anos, regr.predict(anos), color='blue')
- 
+
 plt.xlabel('Anos')
 plt.ylabel('Casos de dengue')
 plt.xticks([2018])
-plt.yticks([int(casos_2018)])
- 
+
+# Correção do aviso de depreciação
+plt.yticks([int(casos_2018[0])])  # Extraído o valor único do array
+
 plt.show()
